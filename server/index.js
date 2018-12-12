88034c69129c7ed8e4ac937ffb870970b29339ef
akoskoc <akoskoc@gmail.com>
2018-12-12 15:09:03 +0100

    bodyparser, cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
+const app = require("express")();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const db = require("./database");
 const tauriApi = require("./tauriApi");
-const app = require("express")();
 const realms = require("../constants/realms");
 const port = 3001;
 
 (async function() {
     await db.connect();
+
+    app.use(
+        cors({
+            origin: "http://localhost:3000",
+            optionsSuccessStatus: 200
+        })
+    );
+
+    app.use(bodyParser.json());
 
     app.get("/", async (req, res) => {
         res.send(await db.getGuilds());
