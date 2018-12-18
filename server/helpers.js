0d2f232a1a934944aba8b37e9932cf20de5a525a
akoskoc <akoskoc@gmail.com>
2018-12-18 16:14:46 +0100

    refactor some names

--- server/helpers.js
+++ server/helpers.js
@@ -12,7 +12,7 @@ async function getRaidBossLogs(bossId, difficulty, lastUpdated = 0) {
     return new Promise(async (resolve, reject) => {
         try {
             let now = new Date().getTime() / 1000;
-            let bossKills = [];
+            let bossLogs = [];
             let logs = [];
             let data;
 
@@ -29,21 +29,21 @@ async function getRaidBossLogs(bossId, difficulty, lastUpdated = 0) {
                 );
                 if (!data.success) throw new Error(data.errorstring);
 
-                bossKills = bossKills.concat(
-                    data.response.logs.map(kill => ({
-                        ...kill,
+                bossLogs = bossLogs.concat(
+                    data.response.logs.map(log => ({
+                        ...log,
                         realm: realms[key]
                     }))
                 );
             }
 
-            for (let kill of bossKills) {
-                if (lastUpdated < kill.killtime) {
+            for (let log of bossLogs) {
+                if (lastUpdated < log.killtime) {
                     let bossData;
                     do {
                         bossData = await tauriApi.getRaidLog(
-                            kill.realm,
-                            kill.log_id
+                            log.realm,
+                            log.log_id
                         );
                     } while (
                         !bossData.success &&
@@ -52,7 +52,7 @@ async function getRaidBossLogs(bossId, difficulty, lastUpdated = 0) {
                     if (!bossData.success)
                         throw new Error(bossData.errorstring);
 
-                    logs.push({ ...bossData.response, realm: kill.realm });
+                    logs.push({ ...bossData.response, realm: log.realm });
                 }
             }
 
@@ -98,11 +98,12 @@ function getDps({ dmg_done }, { fight_time }) {
     return dmg_done / (fight_time / 1000);
 }
 
-function memberDps(member, kill, difficulty) {
+function memberDps(realm, member, kill, difficulty) {
     return {
         race: member.race,
         spec: specs[member.spec],
         class: specToClass[member.spec],
+        realm: realm,
         difficulty: difficulty,
         ilvl: member.ilvl,
         date: kill.killtime,
@@ -115,11 +116,12 @@ function getHps({ heal_done, absorb_done }, { fight_time }) {
     return (heal_done + absorb_done) / (fight_time / 1000);
 }
 
-function memberHps(member, kill, difficulty) {
+function memberHps(realm, member, kill, difficulty) {
     return {
         race: member.race,
         spec: specs[member.spec],
         class: specToClass[member.spec],
+        realm: realm,
         difficulty: difficulty,
         ilvl: member.ilvl,
         date: kill.killtime,
@@ -149,7 +151,7 @@ function processRaidBossLogs({ lastUpdated, logs, difficulty }) {
         if (log.guildid) {
             // if the kill is done by a guild
 
-            let guildId = log.guilddata.name + log.realm;
+            let guildId = `${log.realm} ${log.guilddata.name}`;
             if (!guilds[guildId]) {
                 // if there is no guild data yet create it
                 guilds[guildId] = createGuildBossKill(log);
@@ -158,13 +160,14 @@ function processRaidBossLogs({ lastUpdated, logs, difficulty }) {
             }
 
             for (let member of log.members) {
-                // go thru members of the kill and update dps/hps values
+                let memberId = `${log.realm} ${member.name}`;
                 if (member.dmg_done !== 0) {
                     if (
-                        !raidBoss.dps[member.name] ||
-                        raidBoss.dps[member.name].dps < getDps(member, log)
+                        !raidBoss.dps[memberId] ||
+                        raidBoss.dps[memberId].dps < getDps(member, log)
                     ) {
-                        raidBoss.dps[member.name] = memberDps(
+                        raidBoss.dps[memberId] = memberDps(
+                            log.realm,
                             member,
                             log,
                             difficulty
@@ -172,11 +175,10 @@ function processRaidBossLogs({ lastUpdated, logs, difficulty }) {
                     }
 
                     if (
-                        !guilds[guildId].dps[member.name] ||
-                        guilds[guildId].dps[member.name].dps <
-                            getDps(member, log)
+                        !guilds[guildId].dps[memberId] ||
+                        guilds[guildId].dps[memberId].dps < getDps(member, log)
                     ) {
-                        guilds[guildId].dps[member.name] = memberDps(
+                        guilds[guildId].dps[memberId] = memberDps(
                             member,
                             log,
                             difficulty
@@ -186,10 +188,11 @@ function processRaidBossLogs({ lastUpdated, logs, difficulty }) {
 
                 if (member.heal_done + member.absorb_done !== 0) {
                     if (
-                        !raidBoss.hps[member.name] ||
-                        raidBoss.hps[member.name].hps < getHps(member, log)
+                        !raidBoss.hps[memberId] ||
+                        raidBoss.hps[memberId].hps < getHps(member, log)
                     ) {
-                        raidBoss.hps[member.name] = memberHps(
+                        raidBoss.hps[memberId] = memberHps(
+                            log.realm,
                             member,
                             log,
                             difficulty
@@ -197,11 +200,11 @@ function processRaidBossLogs({ lastUpdated, logs, difficulty }) {
                     }
 
                     if (
-                        !guilds[guildId].hps[member.name] ||
-                        guilds[guildId].hps[member.name].hps <
-                            getHps(member, log)
+                        !guilds[guildId].hps[memberId] ||
+                        guilds[guildId].hps[memberId].hps < getHps(member, log)
                     ) {
-                        guilds[guildId].hps[member.name] = memberHps(
+                        guilds[guildId].hps[memberId] = memberHps(
+                            log.realm,
                             member,
                             log,
                             difficulty
@@ -212,12 +215,14 @@ function processRaidBossLogs({ lastUpdated, logs, difficulty }) {
         } else {
             // else not guild kill, go thru members, only updating dps/hps of raid boss kill
             for (let member of log.members) {
+                let memberId = `${log.realm} ${member.name}`;
                 if (member.dmg_done !== 0) {
                     if (
-                        !raidBoss.dps[member.name] ||
-                        raidBoss.dps[member.name].dps < getDps(member, log)
+                        !raidBoss.dps[memberId] ||
+                        raidBoss.dps[memberId].dps < getDps(member, log)
                     ) {
-                        raidBoss.dps[member.name] = memberDps(
+                        raidBoss.dps[memberId] = memberDps(
+                            log.realm,
                             member,
                             log,
                             difficulty
@@ -227,10 +232,11 @@ function processRaidBossLogs({ lastUpdated, logs, difficulty }) {
 
                 if (member.heal_done + member.absorb_done !== 0) {
                     if (
-                        !raidBoss.hps[member.name] ||
-                        raidBoss.hps[member.name].hps < getHps(member, log)
+                        !raidBoss.hps[memberId] ||
+                        raidBoss.hps[memberId].hps < getHps(member, log)
                     ) {
-                        raidBoss.hps[member.name] = memberHps(
+                        raidBoss.hps[memberId] = memberHps(
+                            log.realm,
                             member,
                             log,
                             difficulty
