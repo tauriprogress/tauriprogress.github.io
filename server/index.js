2623d9522ea2ea11bdd193ee4d201c194b459bba
akoskoc <akoskoc@gmail.com>
2018-12-20 03:53:53 +0100

    routes

--- server/index.js
+++ server/index.js
@@ -3,9 +3,14 @@ const app = require("express")();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const db = require("./database");
-const tauriApi = require("./tauriApi");
-const realms = require("../constants/realms");
 const port = 3001;
+const {
+    verifyGetGuild,
+    verifyGetPlayer,
+    verifyGetRaid,
+    verifyGetboss
+} = require("./middlewares");
+const tauriApi = require("./tauriApi");
 
 (async function() {
     try {
@@ -18,7 +23,6 @@ const port = 3001;
         db.disconnect().catch(err => console.error(err));
         process.exit(1);
     }
-
     app.use(
         cors({
             origin: "http://localhost:3000",
@@ -28,8 +32,90 @@ const port = 3001;
 
     app.use(bodyParser.json());
 
-    app.get("/", async (req, res) => {
-        res.send(await db.getGuildList());
+    app.get("/getguildlist", async (req, res) => {
+        try {
+            res.send({
+                success: true,
+                response: await db.getGuildList()
+            });
+        } catch (err) {
+            res.send({
+                success: false,
+                errorstring: err.message
+            });
+        }
+    });
+
+    app.post("/getguild", verifyGetGuild, async (req, res) => {
+        try {
+            res.send({
+                success: true,
+                response: await db.getGuild(req.body.realm, req.body.guildName)
+            });
+        } catch (err) {
+            res.send({
+                success: false,
+                errorstring: err.message
+            });
+        }
+    });
+
+    app.post("/getplayer", verifyGetPlayer, async (req, res) => {
+        try {
+            let player = await tauriApi.getCharacter(
+                req.body.realm,
+                req.body.playerName
+            );
+            if (!player.success) throw new Error(player.errorstring);
+            let performance = await db.getPlayerPerformance({
+                realm: req.body.realm,
+                playerName: req.body.playerName,
+                raidName: req.body.raidName,
+                bossName: req.body.bossName,
+                difficulty: req.body.difficulty
+            });
+            res.send({
+                success: true,
+                response: { ...player.response, progression: performance }
+            });
+        } catch (err) {
+            res.send({
+                success: false,
+                errorstring: err.message
+            });
+        }
+    });
+
+    app.post("/getraid", verifyGetRaid, async (req, res) => {
+        try {
+            res.send({
+                success: true,
+                response: await db.getRaid(req.body.raidName)
+            });
+        } catch (err) {
+            res.send({
+                success: false,
+                errorstring: err.message
+            });
+        }
+    });
+
+    app.post("/getboss", verifyGetboss, async (req, res) => {
+        try {
+            res.send({
+                success: true,
+                response: await db.getRaidBoss(
+                    req.body.raidName,
+                    req.body.bossName,
+                    req.body.difficulty
+                )
+            });
+        } catch (err) {
+            res.send({
+                success: false,
+                errorstring: err.message
+            });
+        }
     });
 
     app.listen(3001, () => console.log(`Server running on port ${port}`));
