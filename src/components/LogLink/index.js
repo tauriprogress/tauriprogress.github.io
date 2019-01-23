d6cf7b1e69dc4923d0586519c1fbd5b10d3dcfbf
akoskoc <akoskoc@gmail.com>
2019-01-23 15:37:14 +0100

    log link tooltip

--- src/components/LogLink/index.js
+++ src/components/LogLink/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Info from "@material-ui/icons/Info";
+import Tooltip from "@material-ui/core/Tooltip";
 
 function LogLink({ logId, realm }) {
     return (
         <span className="textBold ">
-            <Link to={`/log/${logId}?realm=${realm}`}>
-                <Info className="logLink" fontSize="small" />
-            </Link>
+            <Tooltip title={"Logs"}>
+                <Link to={`/log/${logId}?realm=${realm}`}>
+                    <Info className="logLink" fontSize="small" />
+                </Link>
+            </Tooltip>
         </span>
     );
 }
