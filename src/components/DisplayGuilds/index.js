66474611107c6c0533a22fc7bd3eacb17a059fa1
akoskoc <akoskoc@gmail.com>
2019-02-01 04:16:49 +0100

    rank of guild

--- src/components/DisplayGuilds/index.js
+++ src/components/DisplayGuilds/index.js
@@ -44,7 +44,7 @@ class DisplayGuilds extends React.PureComponent {
                             <Table>
                                 <TableHead className="tableHead">
                                     <TableRow>
-                                        <TableCell>Name</TableCell>
+                                        <TableCell>Guild</TableCell>
                                         <TableCell>Realm</TableCell>
                                         <TableCell>Faction</TableCell>
                                         <TableCell>
@@ -53,78 +53,83 @@ class DisplayGuilds extends React.PureComponent {
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {sortByProgression(data).map(guild => {
-                                        return (
-                                            <TableRow key={guild.guildName}>
-                                                <TableCell
-                                                    component="th"
-                                                    scope="row"
-                                                    className="displayGuildsGuildName"
-                                                >
-                                                    <Typography color="inherit">
-                                                        <RouterLink
-                                                            to={`/guild/${
-                                                                guild.guildName
-                                                            }?realm=${
-                                                                guild.realm
-                                                            }`}
-                                                        >
-                                                            <Link
-                                                                component="span"
+                                    {sortByProgression(data).map(
+                                        (guild, index) => {
+                                            return (
+                                                <TableRow key={guild.guildName}>
+                                                    <TableCell
+                                                        component="th"
+                                                        scope="row"
+                                                        className="displayGuildsGuildName"
+                                                    >
+                                                        <Typography color="inherit">
+                                                            <span className="textBold">
+                                                                {index + 1}.{" "}
+                                                            </span>
+                                                            <RouterLink
+                                                                to={`/guild/${
+                                                                    guild.guildName
+                                                                }?realm=${
+                                                                    guild.realm
+                                                                }`}
+                                                            >
+                                                                <Link
+                                                                    component="span"
+                                                                    style={{
+                                                                        color: guild.gFaction
+                                                                            ? factionColors.horde
+                                                                            : factionColors.alliance
+                                                                    }}
+                                                                >
+                                                                    {
+                                                                        guild.guildName
+                                                                    }
+                                                                </Link>
+                                                            </RouterLink>
+                                                        </Typography>
+                                                    </TableCell>
+                                                    <TableCell
+                                                        component="th"
+                                                        scope="row"
+                                                    >
+                                                        {guild.realm}
+                                                    </TableCell>
+                                                    <TableCell
+                                                        component="th"
+                                                        scope="row"
+                                                    >
+                                                        {guild.gFaction
+                                                            ? "Horde"
+                                                            : "Alliance"}
+                                                    </TableCell>
+                                                    <TableCell
+                                                        component="th"
+                                                        scope="row"
+                                                    >
+                                                        {guild.progression
+                                                            .completed ? (
+                                                            <span
                                                                 style={{
-                                                                    color: guild.gFaction
-                                                                        ? factionColors.horde
-                                                                        : factionColors.alliance
+                                                                    color:
+                                                                        progStateColors.defeated
                                                                 }}
                                                             >
-                                                                {
-                                                                    guild.guildName
-                                                                }
-                                                            </Link>
-                                                        </RouterLink>
-                                                    </Typography>
-                                                </TableCell>
-                                                <TableCell
-                                                    component="th"
-                                                    scope="row"
-                                                >
-                                                    {guild.realm}
-                                                </TableCell>
-                                                <TableCell
-                                                    component="th"
-                                                    scope="row"
-                                                >
-                                                    {guild.gFaction
-                                                        ? "Horde"
-                                                        : "Alliance"}
-                                                </TableCell>
-                                                <TableCell
-                                                    component="th"
-                                                    scope="row"
-                                                >
-                                                    {guild.progression
-                                                        .completed ? (
-                                                        <span
-                                                            style={{
-                                                                color:
-                                                                    progStateColors.defeated
-                                                            }}
-                                                        >
-                                                            {new Date(
-                                                                guild
-                                                                    .progression
-                                                                    .completed *
-                                                                    1000
-                                                            ).toLocaleDateString()}
-                                                        </span>
-                                                    ) : (
-                                                        guild.progression
-                                                            .currentBossesDefeated
-                                                    )}
-                                                </TableCell>
-                                            </TableRow>
-                                        );
-                                    })}
+                                                                {new Date(
+                                                                    guild
+                                                                        .progression
+                                                                        .completed *
+                                                                        1000
+                                                                ).toLocaleDateString()}
+                                                            </span>
+                                                        ) : (
+                                                            guild.progression
+                                                                .currentBossesDefeated
+                                                        )}
+                                                    </TableCell>
+                                                </TableRow>
+                                            );
+                                        }
+                                    )}
                                 </TableBody>
                             </Table>
                         </div>
