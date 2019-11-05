b786bdea3f657056f54f5270ab6f1ec143d8962d
akoskoc <akoskoc@gmail.com>
2019-11-05 23:08:35 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69a4237defb58c4b07db54892d9651ee.js"
+  "/precache-manifest.314b6e319458975a5916ac244515ec74.js"
 );
 
 workbox.clientsClaim();
