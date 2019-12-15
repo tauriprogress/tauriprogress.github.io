cc00a8e89fa6ac6e46d57b0c1eaee1b361acb050
akoskoc <akoskoc@gmail.com>
2019-12-15 22:30:18 +0100

    perfchartcontainer extends props

--- src/components/PerfChart/PerfChartContainer.js
+++ src/components/PerfChart/PerfChartContainer.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 
-function PerfChartContainer({ children }) {
-    return <Container>{children}</Container>;
+function PerfChartContainer({ children, ...rest }) {
+    return <Container {...rest}>{children}</Container>;
 }
 
 export default PerfChartContainer;
