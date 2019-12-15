e8b46d7f94a9607f1015cdafe31ffd7c0a126612
akoskoc <akoskoc@gmail.com>
2019-12-15 22:31:15 +0100

    specimg extends props

--- src/components/SpecImg/index.js
+++ src/components/SpecImg/index.js
@@ -3,10 +3,10 @@ import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Tooltip from "@material-ui/core/Tooltip";
 
-function SpecImg({ src, title }) {
+function SpecImg({ src, title, ...rest }) {
     return (
         <Tooltip title={title} component="span">
-            <Avatar src={src} />
+            <Avatar src={src} {...rest} />
         </Tooltip>
     );
 }
