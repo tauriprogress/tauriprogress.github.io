83d662184fccc520445fcc449293b74d4563b3f7
akoskoc <akoskoc@gmail.com>
2020-01-04 21:06:00 +0100

    new guild component

--- src/components/Guild/index.js
+++ src/components/Guild/index.js
@@ -2,16 +2,11 @@ import React, { useEffect } from "react";
 
 import { useSelector, useDispatch } from "react-redux";
 
-import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-
-import GuildTitle from "./GuildTitle";
-import GuildProgression from "./GuildProgression";
-import GuildRoster from "./GuildRoster";
-import GuildLatestKills from "./GuildLatestKills";
 import ErrorMessage from "../ErrorMessage";
 import Loading from "../Loading";
+
+import GuildIntroduction from "./GuildIntroduction";
+import GuildProgSummary from "./GuildProgSummary";
 
 import { guildFetch, guildSelectTab } from "../../redux/actions";
 
@@ -19,60 +14,27 @@ function Guild({ match, location }) {
     const urlGuildName = match.params.guildName;
     const urlRealm = new URLSearchParams(location.search).get("realm");
 
-    const { data, loading, error, tab } = useSelector(state => state.guild);
+    const { loading, loaded, error, tab } = useSelector(state => ({
+        loading: state.guild.loading,
+        loaded: state.guild.data ? true : false,
+        error: state.guild.error
+    }));
 
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(guildFetch({ guildName: urlGuildName, realm: urlRealm }));
     }, [urlGuildName, urlRealm]);
-
     return (
-        <section style={{ margin: "0 10px" }}>
+        <section>
             {loading && <Loading />}
 
             {error && <ErrorMessage message={error} />}
 
-            {!loading && !error && data && (
+            {!loading && !error && loaded && (
                 <React.Fragment>
-                    <GuildTitle />
-                    <AppBar position="static" style={{ marginBottom: "3px" }}>
-                        <Tabs
-                            value={tab}
-                            onChange={(e, value) =>
-                                dispatch(guildSelectTab(value))
-                            }
-                            indicatorColor="secondary"
-                            variant="scrollable"
-                            scrollButtons="on"
-                            className="displayGuildNavTabs"
-                        >
-                            <Tab label="Progression" className="tab" />
-                            <Tab label="Latest kills" className="tab" />
-                            <Tab label="Roster" className="tab" />
-                        </Tabs>
-                    </AppBar>
-                    {(() => {
-                        switch (tab) {
-                            case 0:
-                                return (
-                                    <GuildProgression
-                                        progression={data.progression}
-                                    />
-                                );
-                            case 1:
-                                return (
-                                    <GuildLatestKills
-                                        data={data.progression.latestKills}
-                                        realm={data.realm}
-                                    />
-                                );
-                            case 2:
-                                return <GuildRoster data={data.guildList} />;
-                            default:
-                                return null;
-                        }
-                    })()}
+                    <GuildIntroduction />
+                    <GuildProgSummary />
                 </React.Fragment>
             )}
         </section>
