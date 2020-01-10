8a790335d639094b05d9ae8b85a26feea641847a
akoskoc <akoskoc@gmail.com>
2020-01-10 04:07:01 +0100

    sidecard component

---
+++ src/components/SideCard/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { withStyles } from "@material-ui/core/styles";
+
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+
+function styles(theme) {
+    return {
+        title: {
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.primary.contrastText,
+            borderRadius: "4px",
+            padding: theme.spacing(1)
+        },
+        container: {
+            margin: `0 ${theme.spacing(1)}px`,
+            width: "260px"
+        }
+    };
+}
+
+function SideCard({ classes, title, children, ...rest }) {
+    return (
+        <Card {...rest} className={`${classes.container} ${rest.className}`}>
+            <Typography className={classes.title}>{title}</Typography>
+            {children}
+        </Card>
+    );
+}
+
+export default withStyles(styles)(SideCard);
