bb8a82bb6f36f4fb94aa72ca01225bf0c01f7087
akoskoc <akoskoc@gmail.com>
2020-01-11 02:31:49 +0100

    guild latest kil with new component

--- src/components/Guild/GuildLatestKills.js
+++ src/components/Guild/GuildLatestKills.js
@@ -3,26 +3,9 @@ import React from "react";
 
 import { withStyles } from "@material-ui/core/styles";
 
-import { Link as RouterLink } from "react-router-dom";
-
-import Typography from "@material-ui/core/Typography";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Divider from "@material-ui/core/Divider";
-
-import CollapsableList from "../CollapseableList";
+import LatestKills from "../LatestKills";
 
 import { useSelector } from "react-redux";
-import {
-    days,
-    getLatestWednesday,
-    dateTextByWeek,
-    dateToString,
-    dateTextHours
-} from "../../helpers";
-import SideCard from "../SideCard";
 
 function styles(theme) {
     return {
@@ -32,17 +15,6 @@ function styles(theme) {
                 marginTop: theme.spacing(1)
             },
             height: "480px"
-        },
-        listItem: {
-            display: "block",
-            padding: 0
-        },
-        list: {
-            maxHeight: "450px",
-            overflowY: "scroll"
-        },
-        bossName: {
-            fontWeight: "bold"
         }
     };
 }
@@ -53,129 +25,12 @@ function GuildLastestKills({ classes }) {
         realm: state.guild.data.realm
     }));
 
-    const currentTime = new Date().getTime();
-    const week = 1000 * 60 * 60 * 24 * 7;
-
-    let dates = {};
-    let dateArray = [];
-
-    for (let log of latestKills) {
-        const logDate = new Date(log.killtime * 1000);
-        const latestWednesDay = getLatestWednesday(logDate);
-        const weeksAgo = Math.floor(
-            (currentTime - latestWednesDay.getTime()) / week
-        );
-        const dateText = dateTextByWeek(weeksAgo);
-
-        if (!dates[dateText])
-            dates[dateText] = {
-                text: dateText,
-                kills: []
-            };
-
-        dates[dateText].kills.push({
-            ...log,
-            dateText: dateToString(logDate),
-            dateDay: days[logDate.getDay()],
-            dateHours: dateTextHours(logDate)
-        });
-    }
-
-    for (let date in dates) {
-        dateArray.push(dates[date]);
-    }
-
     return (
-        <SideCard title={"Latest Kills"} className={classes.container}>
-            <List component="div" disablePadding className={classes.list}>
-                {dateArray.map((date, index) => (
-                    <ListItem
-                        component="div"
-                        className={classes.listItem}
-                        key={date.text}
-                        disableGutters
-                    >
-                        <CollapsableList
-                            defaultState={!index ? true : false}
-                            listTitle={
-                                <Typography
-                                    variant="button"
-                                    className={classes.title}
-                                >
-                                    {date.text}
-                                </Typography>
-                            }
-                        >
-                            {date.kills.map(log => (
-                                <Link
-                                    component={RouterLink}
-                                    color="inherit"
-                                    to={`/log/${log.log_id}?realm=${realm}`}
-                                    rel="noopener noreferrer"
-                                    target="_blank"
-                                    key={log.log_id}
-                                >
-                                    <ListItem component="li" disableGutters>
-                                        <Grid
-                                            container
-                                            wrap="nowrap"
-                                            direction="column"
-                                        >
-                                            <Grid item>
-                                                <Typography
-                                                    className={classes.bossName}
-                                                >
-                                                    {
-                                                        log.encounter_data
-                                                            .encounter_name
-                                                    }
-                                                </Typography>
-                                            </Grid>
-                                            <Grid item>
-                                                <Typography
-                                                    variant="caption"
-                                                    color="textSecondary"
-                                                >
-                                                    {
-                                                        difficultyLabels[
-                                                            log.difficulty
-                                                        ]
-                                                    }
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                        <Grid
-                                            container
-                                            wrap="nowrap"
-                                            direction="column"
-                                        >
-                                            <Grid item>
-                                                <Typography align="right">
-                                                    {log.dateDay}
-                                                    <br />
-                                                    {log.dateHours}
-                                                </Typography>
-                                            </Grid>
-                                            <Grid item>
-                                                <Typography
-                                                    variant="caption"
-                                                    color="textSecondary"
-                                                    align="right"
-                                                    component="p"
-                                                >
-                                                    {log.dateText}
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </ListItem>
-                                    <Divider />
-                                </Link>
-                            ))}
-                        </CollapsableList>
-                    </ListItem>
-                ))}
-            </List>
-        </SideCard>
+        <LatestKills
+            logs={latestKills}
+            realm={realm}
+            className={classes.container}
+        />
     );
 }
 
