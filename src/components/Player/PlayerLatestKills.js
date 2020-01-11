03af1d64ee67e73a1530d654c9e53c4cb87ccc07
akoskoc <akoskoc@gmail.com>
2020-01-11 02:32:04 +0100

    player latest kills with new component

--- src/components/Player/PlayerLatestKills.js
+++ src/components/Player/PlayerLatestKills.js
@@ -1,39 +1,16 @@
-import { difficultyLabels } from "tauriprogress-constants";
 import React from "react";
 import { useSelector } from "react-redux";
 
-import { Link as RouterLink } from "react-router-dom";
-
 import { withStyles } from "@material-ui/core/styles";
-
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 
 import Loading from "../Loading";
 import ErrorMessage from "../ErrorMessage";
-import SideCard from "../SideCard";
-
-import DisplayDate from "../DisplayDate";
+import LatestKills from "../LatestKills";
 
 function styles(theme) {
     return {
-        title: {
-            backgroundColor: theme.palette.primary.main,
-            "& *": {
-                color: `${theme.palette.primary.contrastText} !important`
-            },
-            borderRadius: "4px"
-        },
-        bossName: {
-            fontWeight: "bold"
-        },
         container: {
-            margin: "0 10px",
-            width: "260px"
+            height: "600px"
         }
     };
 }
@@ -45,74 +22,21 @@ function PlayerLatestKills({ classes }) {
     const realm = useSelector(state => state.player.realm);
 
     return (
-        <SideCard title={"Latest Kills"}>
-            <List>
-                {loading && <Loading />}
-                {error && <ErrorMessage message={error} />}
-                {data &&
-                    data.logs.map(log => {
-                        const date = new Date(log.killtime * 1000);
-                        return (
-                            <React.Fragment key={log.log_id}>
-                                <Link
-                                    component={RouterLink}
-                                    color="inherit"
-                                    to={`/log/${log.log_id}?realm=${realm}`}
-                                    rel="noopener noreferrer"
-                                    target="_blank"
-                                >
-                                    <ListItem>
-                                        <Grid container direction="column">
-                                            <Grid item>
-                                                <Typography
-                                                    className={classes.bossName}
-                                                >
-                                                    {`${
-                                                        log.encounter_data
-                                                            .encounter_name
-                                                    } ${
-                                                        difficultyLabels[
-                                                            log.difficulty
-                                                        ]
-                                                    }`}
-                                                </Typography>
-                                            </Grid>
-                                            <Grid item>
-                                                <Grid
-                                                    container
-                                                    wrap="nowrap"
-                                                    justify="space-between"
-                                                >
-                                                    <Grid item>
-                                                        <Typography>
-                                                            <DisplayDate
-                                                                date={date}
-                                                            />
-                                                        </Typography>
-                                                    </Grid>
-                                                    <Grid item>
-                                                        <Typography align="right">
-                                                            {`${(
-                                                                "0" +
-                                                                date.getHours()
-                                                            ).slice(-2)}:${(
-                                                                "0" +
-                                                                date.getMinutes()
-                                                            ).slice(-2)}`}
-                                                        </Typography>
-                                                    </Grid>
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                    </ListItem>
-                                </Link>
-
-                                <Divider />
-                            </React.Fragment>
-                        );
-                    })}
-            </List>
-        </SideCard>
+        <LatestKills
+            logs={data ? data.logs : []}
+            realm={realm}
+            className={classes.container}
+        >
+            {(() => {
+                if (loading) {
+                    return <Loading />;
+                } else if (error) {
+                    return <ErrorMessage message={error} />;
+                } else {
+                    return null;
+                }
+            })()}
+        </LatestKills>
     );
 }
 
