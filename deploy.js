339d2c39fd52265ea2309288b57b113e77366c70
akoskoc <akoskoc@gmail.com>
2020-01-12 05:55:42 +0100

    add google analytics

--- deploy.js
+++ deploy.js
@@ -2,6 +2,8 @@ const util = require("util");
 const fs = require("fs-extra");
 const exec = util.promisify(require("child_process").exec);
 
+const googlAnalitycs =
+    "<!-- Global site tag (gtag.js) - Google Analytics --><script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-156000531-1\"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-156000531-1');</script>";
 const spaScript =
     '<script type="text/javascript">(function(l) { if (l.search) { var q = {}; l.search.slice(1).split("&").forEach(function(v) {var a = v.split("=");q[a[0]] = a.slice(1).join("=").replace(/~and~/g, "&");});if (q.p !== undefined) {window.history.replaceState(null,null,l.pathname.slice(0, -1) +(q.p || "") +(q.q ? "?" + q.q : "") +l.hash);}}})(window.location);</script>';
 const urlsPath = "./node_modules/tauriprogress-constants/urls.json";
@@ -46,10 +48,12 @@ const oldBuildFiles = [
         const indexHtml = fs.readFileSync("index.html", "utf8");
         fs.writeFileSync(
             "index.html",
-            indexHtml.replace(
-                '<div id="root"></div>',
-                `<div id="root"></div>${spaScript}`
-            )
+            indexHtml
+                .replace(
+                    '<div id="root"></div>',
+                    `<div id="root"></div>${spaScript}`
+                )
+                .replace("<head>", `<head>${googlAnalitycs}`)
         );
 
         console.log("Git commit build");
