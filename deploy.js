5b03ff064b1c387b6d817cb3d9b60371acf3de2b
akoskoc <akoskoc@gmail.com>
2020-03-08 15:03:17 +0100

    update package

--- deploy.js
+++ deploy.js
@@ -7,7 +7,7 @@ const googlAnalitycs =
 const spaScript =
     '<script type="text/javascript">(function(l) { if (l.search) { var q = {}; l.search.slice(1).split("&").forEach(function(v) {var a = v.split("=");q[a[0]] = a.slice(1).join("=").replace(/~and~/g, "&");});if (q.p !== undefined) {window.history.replaceState(null,null,l.pathname.slice(0, -1) +(q.p || "") +(q.q ? "?" + q.q : "") +l.hash);}}})(window.location);</script>';
 const urlsPath = "./node_modules/tauriprogress-constants/urls.json";
-const serverUrl = "https://ossified-hyacinth.glitch.me";
+const serverUrl = "https://tauriprogress-server.glitch.me";
 const oldBuildFiles = [
     "static",
     "manifest.json",
