b22b3c4893d1e53b2ab50812c8eeb82e99dc848f
akoskoc <akoskoc@gmail.com>
2020-11-04 13:10:02 +0100

    page titles

--- src/components/Router/RouteSwitch.js
+++ src/components/Router/RouteSwitch.js
@@ -3,6 +3,7 @@ import { Route, Switch, useLocation, Redirect } from "react-router-dom";
 
 import AppContainer from "../AppContainer";
 
+import Page from "../Page";
 import Raid from "../Raid";
 import Guild from "../Guild";
 import Character from "../Character";
@@ -19,19 +20,52 @@ function RouteSwitch() {
     return (
         <AppContainer>
             <Switch location={background || location}>
-                <Route exact path="/" render={() => <GuildList />} />
+                <Route
+                    exact
+                    path="/"
+                    render={props => (
+                        <Page title={"Tauri Progress"}>
+                            <GuildList {...props} />
+                        </Page>
+                    )}
+                />
                 <Route
                     exact
                     path="/raid/:raidName/:bossName?"
-                    component={Raid}
+                    component={props => (
+                        <Page
+                            title={`${
+                                props.match.params.bossName ||
+                                props.match.params.raidName
+                            } | Tauri Progress`}
+                        >
+                            <Raid {...props} />
+                        </Page>
+                    )}
                 />
                 <Route exact path="/guild/:guildName" component={Guild} />
                 <Route
                     exact
                     path="/character/:characterName"
-                    component={Character}
+                    component={props => (
+                        <Page
+                            title={`${props.match.params.characterName} | Tauri Progress`}
+                        >
+                            <Character {...props} />
+                        </Page>
+                    )}
                 />
-                <Route exact path="/log/:logId" component={FightLog} />
+                <Route
+                    exact
+                    path="/log/:logId"
+                    component={props => (
+                        <Page
+                            title={`Log ${props.match.params.logId} | Tauri Progress`}
+                        >
+                            <FightLog {...props} />
+                        </Page>
+                    )}
+                />
                 <Route exact path="/secret" component={TauriApi} />
                 <Route
                     path="/player"
@@ -44,10 +78,26 @@ function RouteSwitch() {
                         />
                     )}
                 />
-                <Route component={NotFound} />
+                <Route
+                    component={props => (
+                        <Page title={`Not Found | Tauri Progress`}>
+                            <NotFound {...props} />
+                        </Page>
+                    )}
+                />
             </Switch>
             {background && (
-                <Route exact path="/log/:logId" component={ModalFightLog} />
+                <Route
+                    exact
+                    path="/log/:logId"
+                    component={props => (
+                        <Page
+                            title={`Log ${props.match.params.logId} | Tauri Progress`}
+                        >
+                            <ModalFightLog {...props} />
+                        </Page>
+                    )}
+                />
             )}
         </AppContainer>
     );
