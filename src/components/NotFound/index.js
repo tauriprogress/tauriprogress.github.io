55af386b2c2c324e0171f72d5ca791dd27d8096f
akoskoc <akoskoc@gmail.com>
2021-06-04 13:07:49 +0200

    modify not found

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -8,7 +8,7 @@ function NotFound() {
         <Page title={`Not Found | Tauri Progress`}>
             <section className="notFound">
                 <Typography variant="h1">404</Typography>
-                <Typography variant="h6">You seem te be lost. :/</Typography>
+                <Typography variant="h6">Not found</Typography>
             </section>
         </Page>
     );
