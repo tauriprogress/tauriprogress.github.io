c02c0c89870e742b07723b27be052e47ea6be637
akoskoc <akoskoc@gmail.com>
2021-06-04 12:47:06 +0200

    display million to 2 decimal

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -185,7 +185,7 @@ export function shortNumber(number) {
     } else if (number < 1000 * 1000) {
         return `${(Math.round(number / 100) / 10).toFixed(1)}K`;
     } else {
-        return `${(Math.round(number / 10000) / 100).toFixed(1)}M`;
+        return `${(Math.round(number / 10000) / 100).toFixed(2)}M`;
     }
 }
 
