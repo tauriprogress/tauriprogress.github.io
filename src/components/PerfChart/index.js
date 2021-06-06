d3ed56e955d4586acb1c14203bb43a54d1cd9962
akoskoc <akoskoc@gmail.com>
2021-06-06 19:30:31 +0200

    fix warning

--- src/components/PerfChart/index.js
+++ src/components/PerfChart/index.js
@@ -2,6 +2,8 @@ import PerfChartContainer from "./PerfChartContainer";
 import PerfChartTitle from "./PerfChartTitle";
 import PerfChartRow from "./PerfChartRow";
 
+const exports = { PerfChartContainer, PerfChartTitle, PerfChartRow };
+
 export { PerfChartContainer, PerfChartTitle, PerfChartRow };
 
-export default { PerfChartContainer, PerfChartTitle, PerfChartRow };
+export default exports;
