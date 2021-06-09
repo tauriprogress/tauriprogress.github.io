fa9e3f9be90955d7613f74ca5d1a20d21ff8c92c
akoskoc <akoskoc@gmail.com>
2021-06-09 19:31:52 +0200

    filter in url for raid

--- src/components/Raid/index.js
+++ src/components/Raid/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import queryString from "query-string";
 
 import Page from "../Page";
 import RaidSummary from "../RaidSummary";
@@ -7,6 +9,16 @@ import RaidBoss from "../RaidBoss";
 import RaidFilter from "../RaidFilter";
 
 function Raid({ match }) {
+    const filter = queryString.stringify(
+        useSelector(state => state.raid.filter)
+    );
+    if (filter !== window.location.search) {
+        window.history.replaceState(
+            window.history.state,
+            document.title,
+            `${window.location.origin}${window.location.pathname}?${filter}`
+        );
+    }
     return (
         <Page
             title={`${
--- src/redux/reducers/raidReducer.js
+++ src/redux/reducers/raidReducer.js
@@ -1,20 +1,54 @@
-import { getDefaultDifficulty } from "../../helpers";
+import { characterSpecToClass } from "tauriprogress-constants";
+import queryString from "query-string";
+import {
+    getDefaultDifficulty,
+    validRealmGroup,
+    validDifficulty,
+    validClass,
+    validFaction,
+    validRealm,
+    validRole,
+    validSpec
+} from "../../helpers";
 
-const defaultRealmGroup = localStorage.getItem("realmGroup") || "tauri";
+const defaultRealmGroup = validRealmGroup(localStorage.getItem("realmGroup"))
+    ? localStorage.getItem("realmGroup")
+    : "tauri";
 
 const defaultDifficulty = getDefaultDifficulty(defaultRealmGroup);
+
+const filterFromUrl = queryString.parse(window.location.search);
 
 const defaultState = {
     selected: 0,
     error: null,
     loading: false,
     filter: {
-        difficulty: defaultDifficulty,
-        class: "",
-        spec: "",
-        role: "",
-        faction: "",
-        realm: ""
+        difficulty: validDifficulty(
+            Number(filterFromUrl.difficulty),
+            defaultRealmGroup
+        )
+            ? Number(filterFromUrl.difficulty)
+            : defaultDifficulty,
+        faction:
+            filterFromUrl.faction !== "" &&
+            validFaction(Number(filterFromUrl.faction))
+                ? Number(filterFromUrl.faction)
+                : "",
+        class: validClass(filterFromUrl.class, defaultRealmGroup)
+            ? filterFromUrl.class
+            : "",
+        spec:
+            validClass(filterFromUrl.class, defaultRealmGroup) &&
+            validSpec(filterFromUrl.spec, defaultRealmGroup) &&
+            characterSpecToClass[filterFromUrl.spec] ===
+                Number(filterFromUrl.class)
+                ? filterFromUrl.spec
+                : "",
+        role: validRole(filterFromUrl.role) ? filterFromUrl.role : "",
+        realm: validRealm(filterFromUrl.realm, defaultRealmGroup)
+            ? filterFromUrl.realm
+            : ""
     }
 };
 
