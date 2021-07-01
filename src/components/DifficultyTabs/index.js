9e373da07b083d3f45135a8e552ab66dc4f0c81e
akoskoc <akoskoc@gmail.com>
2021-07-01 08:01:08 +0200

    char prog diff tab display styles

--- src/components/DifficultyTabs/index.js
+++ src/components/DifficultyTabs/index.js
@@ -1,40 +1,33 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import { withStyles } from "@material-ui/core/styles";
-
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 
-function styles(theme) {
-    return {
-        tabs: {
-            width: "160px"
-        }
-    };
-}
-
-function DifficultyTabs({ classes, options, selected, onChange }) {
+function DifficultyTabs({ options, selected, onChange }) {
     const difficultyNames = useSelector(
         state => state.environment.difficultyNames
     );
+
     return (
         <Tabs
             onChange={onChange}
             value={selected}
             variant="fullWidth"
-            className={classes.tabs}
+            style={{
+                width: `${80 * options.length}px`,
+                maxWidth: `calc(100vw)`
+            }}
         >
             {options.map(difficulty => (
                 <Tab
                     key={difficulty}
                     value={difficulty}
                     label={difficultyNames[difficulty]}
-                    className={classes.tab}
                 />
             ))}
         </Tabs>
     );
 }
 
-export default withStyles(styles)(DifficultyTabs);
+export default DifficultyTabs;
