d2ccaf0bc6a53f52fe253c24d52ade39064eef08
akoskoc <akoskoc@gmail.com>
2022-04-22 09:11:50 +0200

    fix minor bug

--- src/components/Character/helpers.js
+++ src/components/Character/helpers.js
@@ -1,6 +1,10 @@
 export function displayHealing(bosses) {
     for (let bossName in bosses) {
-        if (bosses[bossName]["all"] && bosses[bossName]["all"].hps.hps) {
+        if (
+            bosses[bossName]["all"] &&
+            bosses[bossName]["all"].hps &&
+            bosses[bossName]["all"].hps.hps
+        ) {
             return true;
         }
     }
