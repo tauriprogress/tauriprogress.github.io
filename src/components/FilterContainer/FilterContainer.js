641b6a893178b18486605a608c34e6e2847a98fd
akoskoc <akoskoc@gmail.com>
2022-04-28 14:19:09 +0200

    refactor filter container

--- src/components/FilterContainer/FilterContainer.js
+++ src/components/FilterContainer/FilterContainer.js
@@ -1,24 +1,14 @@
-import React from "react";
-
-import { withStyles } from "@mui/styles";
 import Container from "@mui/material/Container";
+import { styled } from "@mui/styles";
 
-function styles(theme) {
-    return {
-        container: {
-            padding: 0,
-            display: "flex",
-            justifyContent: "center",
-            flexWrap: "wrap",
-            "& .MuiFormControl-root": {
-                margin: `0 ${theme.spacing(1)}`
-            }
-        }
-    };
-}
+const FilterContainer = styled(Container)(({ theme }) => ({
+    padding: 0,
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    "& .MuiFormControl-root": {
+        margin: theme.spacing(1),
+    },
+}));
 
-function FilterContainer({ classes, children }) {
-    return <Container className={classes.container}>{children}</Container>;
-}
-
-export default withStyles(styles)(FilterContainer);
+export default FilterContainer;
