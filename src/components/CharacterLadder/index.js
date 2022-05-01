1320d48aa030116fb001a0dd0e6d26f4a60980ed
akoskoc <akoskoc@gmail.com>
2022-05-01 11:29:27 +0200

    refactor table pagination action

--- src/components/CharacterLadder/index.js
+++ src/components/CharacterLadder/index.js
@@ -10,17 +10,11 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import TablePagination from "@mui/material/TablePagination";
 import Tooltip from "@mui/material/Tooltip";
-import Box from "@mui/material/Box";
 
 import CharacterList from "./CharacterList";
 
-import IconButton from "@mui/material/IconButton";
-import FirstPageIcon from "@mui/icons-material/FirstPage";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import LastPageIcon from "@mui/icons-material/LastPage";
-
 import InfoIcon from "../InfoIcon";
+import TablePaginationActions from "../TablePaginationActions";
 
 import OverflowScroll from "../OverflowScroll";
 import ElevatedLinearProgress from "../ElevatedLinearProgress";
@@ -28,59 +22,6 @@ import ElevatedLinearProgress from "../ElevatedLinearProgress";
 const UppercaseTableCell = styled(TableCell)({
     textTransform: "uppercase",
 });
-
-function TablePaginationActions(props) {
-    const { count, page, rowsPerPage, onPageChange } = props;
-
-    const handleFirstPageButtonClick = (event) => {
-        onPageChange(event, 0);
-    };
-
-    const handleBackButtonClick = (event) => {
-        onPageChange(event, page - 1);
-    };
-
-    const handleNextButtonClick = (event) => {
-        onPageChange(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (event) => {
-        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-
-    return (
-        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-            <IconButton
-                onClick={handleFirstPageButtonClick}
-                disabled={page === 0}
-                aria-label="first page"
-            >
-                <FirstPageIcon />
-            </IconButton>
-            <IconButton
-                onClick={handleBackButtonClick}
-                disabled={page === 0}
-                aria-label="previous page"
-            >
-                <KeyboardArrowLeft />
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                <KeyboardArrowRight />
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="last page"
-            >
-                <LastPageIcon />
-            </IconButton>
-        </Box>
-    );
-}
 
 function CharacterLadder({
     filter = {},
