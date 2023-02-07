1a53488ec3f60e071238fac758843219caa5beb7
akoskoc <akoskoc@gmail.com>
2023-02-07 22:32:16 +0100

    remove seasonal countdown

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,12 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 import GuildList from "../GuildList";
-import SeasonalCountdown from "../SeasonalCountdown";
 import Page from "../Page";
 
-import { environmentEntireSeasonalSelector } from "../../redux/selectors";
-
 function Home() {
-    const { isSeasonal, isSeasonRunning } = useSelector(
-        environmentEntireSeasonalSelector
-    );
-
     return (
         <Page>
-            {isSeasonal && !isSeasonRunning ? (
-                <SeasonalCountdown />
-            ) : (
-                <GuildList />
-            )}
+            <GuildList />
         </Page>
     );
 }
