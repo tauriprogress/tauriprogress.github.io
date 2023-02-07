0edde660e1ce3d464332579c87999fecc59538e6
akoskoc <akoskoc@gmail.com>
2023-02-07 17:30:36 +0100

    refactor realmgroup change

--- src/components/Header/LeftNavItems.js
+++ src/components/Header/LeftNavItems.js
@@ -1,84 +1,29 @@
 import React from "react";
-import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import MenuIcon from "@mui/icons-material/Menu";
 import IconButton from "@mui/material/IconButton";
-import Button from "@mui/material/Button";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import FormControl from "@mui/material/FormControl";
-import { TallGrid, VeritcalCenterGrid, rightNavBreakpoint } from "./";
-import { push } from "connected-react-router";
+import { TallGrid, VeritcalCenterGrid } from "./";
 
-import {
-    navigationToggle,
-    environmentSetRealmGroup,
-    environmentToggleSeason,
-} from "../../redux/actions";
+import { navigationToggle } from "../../redux/actions";
 
-import {
-    environmentIsSeasonalSelector,
-    environmentNextSeasonNameSelector,
-    environmentSeasonNameSelector,
-    environmentRealmGroupSelector,
-    environmentHasSeasonalSelector,
-} from "../../redux/selectors";
 import { getRealmGroupsMetaData, getExpansionImg } from "../../helpers";
 
-import { styled } from "@mui/system";
 import { Avatar } from "@mui/material";
-
-const SeasonButton = styled(Button)(({ theme }) => ({
-    padding: 10,
-    height: "100%",
-    fontSize: `${18 / 16}rem`,
-    position: "relative",
-    color: theme.palette.secondary.main,
-    "& :hover": {
-        color: theme.palette.secondary.main,
-    },
-    [`@media(max-width: ${rightNavBreakpoint})`]: {
-        maxWidth: "30px !important",
-    },
-}));
-
-const DisabledSpan = styled("span")(({ theme }) => ({
-    color: theme.palette.text.disabled,
-    display: "flex",
-    maxWidth: "100%",
-}));
-
-const Span = styled("span")({
-    display: "flex",
-    maxWidth: "100%",
-});
-
-const ButtonText = styled("span")({
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    maxWidth: "200px",
-    whiteSpace: "pre",
-});
+import { useHistory, useParams } from "react-router-dom";
 
 function LeftNavItems() {
     const dispatch = useDispatch();
-    const { realmGroup, hasSeasonal, seasonName, nextSeasonName, isSeasonal } =
-        useSelector(
-            (state) => ({
-                realmGroup: environmentRealmGroupSelector(state),
-                hasSeasonal: environmentHasSeasonalSelector(state),
-                seasonName: environmentSeasonNameSelector(state),
-                nextSeasonName: environmentNextSeasonNameSelector(state),
-                isSeasonal: environmentIsSeasonalSelector(state),
-            }),
-            shallowEqual
-        );
+    const { realmGroupName } = useParams();
+    const history = useHistory();
 
-    function seasonalSwitch() {
-        if (hasSeasonal) {
-            dispatch(environmentToggleSeason());
-        }
+    function changeRealmGroup(realmGroup) {
+        history.push(`/${realmGroup}`);
+        localStorage.set("realmGroup", realmGroup);
     }
 
     return (
@@ -97,11 +42,10 @@ function LeftNavItems() {
                 <FormControl>
                     <InputLabel>Server</InputLabel>
                     <Select
-                        value={realmGroup}
+                        value={realmGroupName}
                         label={"Server"}
                         onChange={(e) => {
-                            dispatch(push("/"));
-                            dispatch(environmentSetRealmGroup(e.target.value));
+                            changeRealmGroup(e.target.value);
                         }}
                     >
                         {getRealmGroupsMetaData().map((realmGroupMetaData) => (
@@ -120,26 +64,6 @@ function LeftNavItems() {
                     </Select>
                 </FormControl>
             </VeritcalCenterGrid>
-            {hasSeasonal && (seasonName || nextSeasonName) && (
-                <VeritcalCenterGrid item style={{ maxWidth: "100%" }}>
-                    <SeasonButton onClick={seasonalSwitch}>
-                        {(() => {
-                            const Component = isSeasonal ? Span : DisabledSpan;
-                            let name = seasonName || nextSeasonName;
-
-                            return (
-                                <Component>
-                                    {name.split(" ").map((text) => (
-                                        <ButtonText key={text}>
-                                            {text}{" "}
-                                        </ButtonText>
-                                    ))}
-                                </Component>
-                            );
-                        })()}
-                    </SeasonButton>
-                </VeritcalCenterGrid>
-            )}
         </TallGrid>
     );
 }
