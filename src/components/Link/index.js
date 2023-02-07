5b1baa90daac07204cee119c550628d1d4174c78
akoskoc <akoskoc@gmail.com>
2023-02-07 17:00:31 +0100

    modify link component to use realmgroupname

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,18 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useParams } from "react-router-dom";
 import { Link as MaterialLink } from "@mui/material";
 
-import { environmentIsSeasonalSelector } from "../../redux/selectors";
-
 function Link({ to, children, ...rest }) {
-    const isSeasonal = useSelector(environmentIsSeasonalSelector);
-    const goTo = !isSeasonal
-        ? to
-        : typeof to === "string"
-        ? "/seasonal" + to
-        : { ...to, pathname: "/seasonal" + to.pathname };
+    const { realmGroupName } = useParams();
+
+    const goTo =
+        typeof to === "string"
+            ? `/${realmGroupName}` + to
+            : { ...to, pathname: `/${realmGroupName}` + to.pathname };
     return (
         <MaterialLink
             component={RouterLink}
