64538685ad83a0084ecb2fc8c6eb7eddeb9e8f0e
akoskoc <akoskoc@gmail.com>
2023-02-07 16:53:22 +0100

    validate and redirect to valid realmgroup route

--- src/components/Router/RouteSwitch.js
+++ src/components/Router/RouteSwitch.js
@@ -1,20 +1,45 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import {
+    Route,
+    Switch,
+    matchPath,
+    useLocation,
+    Redirect,
+} from "react-router-dom";
 
 import LogModal from "../Log/Modal";
 
 import ROUTES from "../../routes";
+import { getRealmGroupFromLocalStorage, validRealmGroup } from "../../helpers";
 
 function RouteSwitch() {
-    const location = useSelector(state => state.router.location);
+    const location = useLocation();
 
     const background = location.state && location.state.background;
+
+    const realmGroupName = matchPath(location.pathname, {
+        path: "/:realmGroupName",
+        exact: false,
+        strict: false,
+    }).params.realmGroupName;
+
+    if (!validRealmGroup(realmGroupName)) {
+        const defaultRealmGroup = getRealmGroupFromLocalStorage();
+
+        return (
+            <Redirect
+                to={location.pathname.replace(
+                    realmGroupName,
+                    defaultRealmGroup
+                )}
+            />
+        );
+    }
 
     return (
         <React.Fragment>
             <Switch location={background || location}>
-                {ROUTES.map(route => (
+                {ROUTES.map((route) => (
                     <Route
                         exact={route.exact}
                         key={route.name}
