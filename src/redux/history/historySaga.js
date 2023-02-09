ffa7752ddefc1feff817d73da7e1e068e126561b
akoskoc <akoskoc@gmail.com>
2023-02-09 11:40:55 +0100

    fix url query of raidboss

--- src/redux/history/historySaga.js
+++ src/redux/history/historySaga.js
@@ -8,6 +8,7 @@ import {
     GUILD_LEADERBOARD_FILTER_SET,
     GUILD_LEADERBOARD_TAB_SET,
     HISTORY_PUSH,
+    NAVIGATION_ITEM_SET,
 } from "../actions";
 
 import {
@@ -29,6 +30,7 @@ function* handeHistoryAction({ type, payload }) {
         case HISTORY_PUSH:
             history.push(`/${realmGroupName}${payload}`);
             break;
+        case NAVIGATION_ITEM_SET:
         case RAIDFILTER_SET:
         case RAIDBOSS_TAB_SET:
             history.replace(
@@ -72,6 +74,7 @@ export default function* historySaga() {
             CHARACTER_LEADERBOARD_TAB_SET,
             GUILD_LEADERBOARD_FILTER_SET,
             GUILD_LEADERBOARD_TAB_SET,
+            NAVIGATION_ITEM_SET,
         ],
         handeHistoryAction
     );
