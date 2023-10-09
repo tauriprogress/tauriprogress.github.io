349023fc7717a5eb456dcdd7cc83949d3390c733
akoskoc <akoskoc@gmail.com>
2023-10-09 12:18:47 +0200

    home icon

---
+++ src/components/HomeIcon/index.js
@@ -0,0 +1,13 @@
+import brandIcon from "../../assets/social/brandIcon.svg";
+import { styled } from "@mui/system";
+
+const Img = styled("img")({
+    width: `21px`,
+    height: `21px`,
+});
+
+function HomeIcon() {
+    return <Img src={brandIcon} alt="Home" />;
+}
+
+export default HomeIcon;
